4e9dbabf808a1b01faa57f3d36b9a424c4ff9947
Pavan Gole <64621994+pavangole@users.noreply.github.com>
2023-02-05 03:41:28 +0000

    Doing Analysis

--- Functions.java
+++ Functions.java
@@ -1,12 +1,62 @@
 public class Functions {
     public static void main(String[] args) {
-        String columns = String.format("| %3s | %22s | %22s | %22s | %22s | %22s | %22s | %22s |", "n", "2^n", "n ^ 3",
-                "n . 2 ^ n", " ln n", "(3/2) ^ n", " n log n", " 2 ^ log n");
+        String columns = String.format("| %3s | %22s | %22s | %22s | %22s" +
+                "| %22s | %22s | %22s |",
+                "n", "2^n", "n ^ 3",
+                "n . 2 ^ n", " ln n", "(3/2) ^ n",
+                " n log n", " 2 ^ log n");
         System.out.println(columns);
         for (int i = 0; i < 101; i++) {
-            String output = String.format("| %3s | %22s | %22s | %22s | %22s | %22s | %22s | %22s |",i,Math.pow(2,i),Math.pow(3,i),
-                i * (Math.pow(2,i)), Math.log(i), Math.pow(1.5,i),i * (Math.log(i) / Math.log(2)),Math.pow(2,Math.log(Math.log(i) / Math.log(2))));
-                System.out.println(output);
-        }   
+            String output = String.format("| %3s | %22s | %22s | %22s" +
+                    "| %22s | %22s | %22s | %22s |", i, Math.pow(2, i),
+                    Math.pow(3, i),
+                    i * (Math.pow(2, i)), Math.log(i),
+                    Math.pow(1.5, i), i * (Math.log(i) / Math.log(2)),
+                    Math.pow(2, Math.log(Math.log(i) / Math.log(2))));
+            System.out.println(output);
+        }
+        System.out.println();
+        columns = String.format("| %22s | %22s |",
+                "ln ln n", "log n");
+        System.out.println(columns);
+        for (int i = 0; i < 101; i++) {
+            String output = String.format("| %22s | %22s |",
+                    Math.log(Math.log(i)), Math.log(i) / Math.log(2));
+            System.out.println(output);
+        }
+
+        columns = String.format("| %3s | %22s | %22s | %22s |" +
+                "%22s | %22s | %22s | %22s |", "n", "2^n", "n ^ 3",
+                "n . 2 ^ n", " ln n", "(3/2) ^ n",
+                " n log n", " 2 ^ log n");
+        System.out.println(columns);
+        for (int i = 0; i < 21; i++) {
+            String output = String.format("| %3s | %22s | %22s | %22s " +
+                    "| %22s | %22s | %22s | %22s |",
+                    i, Math.pow(2, i),
+                    Math.pow(3, i),
+                    i * (Math.pow(2, i)), Math.log(i),
+                    Math.pow(1.5, i), i * (Math.log(i) / Math.log(2)),
+                    Math.pow(2, Math.log(Math.log(i) / Math.log(2))));
+            System.out.println(output);
+        }
+        System.out.println();
+        columns = String.format("| %22s | %22s | %22s |", 
+        "ln ln n", "log n", "n!");
+        System.out.println(columns);
+        for (int i = 0; i < 21; i++) {
+            String output = String.format("| %22s | %22s | %22s |",
+                    Math.log(Math.log(i)), Math.log(i) / Math.log(2),
+                    factorial(i));
+            System.out.println(output);
+        }
+
+    }
+
+    static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
 }
--- My.java
+++ My.java
@@ -1,66 +1,57 @@
-import java.util.Arrays;
-
-/**
- * Analysis
- */
-public class Analysis {
+public class My {
+    //create a program to find number of comparisons and swaps in 
 
     public static void main(String[] args) {
-
         int[] input = new int[10];
         for (int i = 0; i < input.length; i++) {
             int r = 3;
             r = r + ((r + 1) * i);
             input[i] = r;
         }
-        System.out.println("For Selection Sort");
-        System.out.println("Best Case");
-        selection(input.clone());
-        System.out.println("Worst Case");
-        int[] temp = new int[10];
-        for (int i = 0, j = 9; i < 10 && j >= 0; i++, j--) {
-            temp[i] = input[j];
-        }
-        selection(temp);
-        System.out.println("Average Case");
-        int average[] = { 3, 39, 7, 35, 11, 31, 15, 27, 19, 23 };
-        selection(average);
-
+        selection(input);
+        insertion(input);
     }
-
     static void selection(int input[]) {
         int comparisons = 0;
         int swaps = 0;
         System.out.print("Input :- ");
-        System.out.println(Arrays.toString(input));
-        for (int i = 0; i < input.length - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < input.length; j++) {
+        for (int j = 0; j < input.length; j++) {
+            System.out.print(input[j] + " ");
+        }
+        for(int i = 0; i < input.length - 1 ; i++) {
+            int min  = i;
+            for(int j = i + 1; j < input.length ; j++) {
                 comparisons++;
-                if (input[j] < input[min]) {
+                if(input[j] < input[min]) {
                     min = j;
                 }
             }
-            if (min != i) {
+            if(min != i) {
                 int temp = input[i];
-                input[i] = input[min];
+                input[i]  = input[min];
                 input[min] = temp;
                 swaps++;
             }
-
+            
+            
         }
+        System.out.println();
         System.out.print("Output :- ");
-        System.out.println(Arrays.toString(input));
+        for (int j = 0; j < input.length; j++) {
+            System.out.print(input[j] + " ");
+        }
+        System.out.println();
         System.out.println("Number of Comparisons :- " + comparisons);
         System.out.println("Number of Swaps :- " + swaps);
         System.out.println();
     }
-
     static void insertion(int input[]) {
         int comparisons = 0;
         int swaps = 0;
         System.out.print("Input :- ");
-        System.out.println(Arrays.toString(input));
+        for (int j = 0; j < input.length; j++) {
+            System.out.print(input[j] + " ");
+        }
         for (int i = 1; i < input.length; i++) {
             int key = input[i];
             int j = i - 1;
@@ -75,11 +66,12 @@ public class Analysis {
         }
         System.out.println();
         System.out.print("Output :- ");
-        System.out.println(Arrays.toString(input));
+        for (int j = 0; j < input.length; j++) {
+            System.out.print(input[j] + " ");
+        }
         System.out.println();
         System.out.println("Number of Comparisons :- " + comparisons);
         System.out.println("Number of Swaps :- " + swaps);
         System.out.println();
     }
-
 }
